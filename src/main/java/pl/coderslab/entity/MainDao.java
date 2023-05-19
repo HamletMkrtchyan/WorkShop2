0e23e2144aeee570b6f5e97e853e5bcef41eb991
hamlet <mkrtchyan@vp.pl>
2023-05-19 17:45:50 +0200

    TaskManager2

--- src/main/java/pl/coderslab/entity/MainDao.java
+++ src/main/java/pl/coderslab/entity/MainDao.java
@@ -1,2 +1,28 @@
-package pl.coderslab.entity;public class MainDao {
+package pl.coderslab.entity;
+
+public class MainDao {
+    public static void main(String[] args) {
+        UserDao userDao = new UserDao();
+//        User user = new User();
+//        user.setUserName("HPoxos");
+//        user.setEmail("poxos");
+//        user.setPassword("pass");
+//        userDao.create(user);
+
+
+//        User read = userDao.read(1);
+//        System.out.println(read);
+//
+//        User readNotExist = userDao.read(2);
+//        System.out.println(readNotExist);
+//
+//        User userToUpdate = userDao.read(1);
+//        userToUpdate.setUserName("kakakaa");
+//        userToUpdate.setEmail("haxjnaos");
+//        userToUpdate.setPassword("sPassword");
+//        userDao.update(userToUpdate);
+
+
+
+    }
 }
